c3a02e8aff59009318619fa769c29ba509fe72d7
Harsh Vardhan <Harshvr003@gmail.com>
2025-04-14 19:13:57 +0530

    added

--- Models/healthData.js
+++ Models/healthData.js
@@ -4,19 +4,15 @@ let healthData = new mongoose.Schema({
     dateOfRecord : {
         type : Date,
         default : Date.now(),
-        required : true
     },
     bodyTemperature : {
         type : String,
-        required : true
     },
     bloodPressure : {
         type : String,
-        required : true
     },
     heartRate : {
         type : String,
-        required : true
     }
 })
 
